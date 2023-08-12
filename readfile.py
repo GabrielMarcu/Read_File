1c80ace5b49fc562c428a9d6fcd08025fd5bd3df
GabrielMarcu <gabriel.marcu93@gmail.com>
2023-08-12 17:09:49 +0300

    first commit

---
+++ readfile.py
@@ -0,0 +1,18 @@
+# Scrie o aplicatie care sa poata fi executata din linie de comanda (terminal), care sa primeasca
+# argumente fisiere text si/sau csv si sa returneze continutul fisierelor in terminal
+
+
+def read_file_csv(path: str) ->list[list]:
+    """Reads a csv file
+    :return : a list of rows of a csv file"""
+    pass
+
+
+def read_file_txt(path: str) -> list:
+    """Reads a text file
+    :return : a list of rows of a text file"""
+    pass
+
+
+if __name__ =="__main__":
+    pass
